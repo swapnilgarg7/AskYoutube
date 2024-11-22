312866d02d90847e0cdf508e64c5c10dd37b2012
swapnilgarg7 <swapnilgarg810@gmail.com>
2024-11-22 23:27:37 +0530

    fixed mobile ytlink bug

--- app/utils/transcript.js
+++ app/utils/transcript.js
@@ -3,7 +3,18 @@
 import { YoutubeTranscript } from 'youtube-transcript';
 
 export const fetchTranscript = async (url) => {
-    const videoId = url.split("v=")[1];
+    let videoId;
+
+    if (url.includes("youtu.be")) {
+        videoId = url.split("/").pop().split("?")[0];
+    } else if (url.includes("youtube.com")) {
+        const params = new URLSearchParams(new URL(url).search);
+        videoId = params.get("v");
+    } else {
+        throw new Error("Invalid YouTube URL");
+    }
+
+    if (!videoId) throw new Error("Video ID could not be extracted");
     const transcript = await YoutubeTranscript.fetchTranscript(videoId);
     console.log(transcript);
     return transcript.map((entry) => entry.text).join(" ");
